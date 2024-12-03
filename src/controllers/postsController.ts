925602c8a5fe2559f7f00662895ea874918e64ff
ian-alemida <iangtaonline1@gmail.com>
2024-12-03 19:49:48 -0300

    tipando controller

--- src/controllers/postsController.ts
+++ src/controllers/postsController.ts
@@ -1,75 +1,84 @@
+import { Request, Response } from "express"
 import {
   getTodosPosts,
   criarPost,
   atualizarPost,
   excluirPost,
+  postInterface,
 } from "../models/postsModel.js";
 import fs from "fs";
 import gerarDescricaoComGemini from "../services/geminiService.js";
+import { InsertOneResult, ObjectId, UpdateResult } from "mongodb";
+// Adiciona o 'file'  do Multer a  tipagem do request             
 
-export async function listarPosts(req, res) {
-  // Chama a função para buscar os posts
-  const posts = await getTodosPosts();
-  // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON
-  res.status(200).json(posts);
+export async function listarPosts(req: Request, res: Response): Promise<void>{
+  try{
+    // Chama a função para buscar os posts
+    const posts: postInterface[] = await getTodosPosts();
+    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON
+    res.status(200).json(posts);
+  } catch (erro: any) {
+    console.error(erro.message);
+    res.status(500).json({Erro: erro.message});
+  }
 }
 
-export async function postarNovoPost(req, res) {
-  const novoPost = req.body;
+export async function postarNovoPost(req: Request, res: Response) {
+  const novoPost: Omit<postInterface, "_id"> = req.body;
   try {
-    const postCriado = await criarPost(novoPost);
+    const postCriado: InsertOneResult<postInterface> = await criarPost(novoPost);
     res.status(200).json(postCriado);
-  } catch (erro) {
+  } catch (erro: any) {
     console.error(erro.message);
     res.status(500).json({ Erro: "Falha na requisição" });
   }
 }
 
-export async function uploadImagem(req, res) {
-  const novoPost = {
+export async function uploadImagem(req: Request, res: Response): Promise<void> {
+  const novoPost: Omit<postInterface, "_id"> = {
     descricao: "",
     ImgUrl: req.file.originalname,
     alt: "",
   };
 
   try {
-    const postCriado = await criarPost(novoPost);
+    const postCriado: InsertOneResult<postInterface> = await criarPost(novoPost);
     const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
     fs.renameSync(req.file.path, imagemAtualizada);
     res.status(200).json(postCriado);
-  } catch (erro) {
+  } catch (erro: any) {
     console.error(erro.message);
     res.status(500).json({ Erro: "Falha na requisição" });
   }
 }
 
-export async function atualizarNovoPost(req, res) {
+export async function atualizarNovoPost(req: Request, res: Response): Promise<void> {
   const id = req.params.id;
   const urlImagem = `http://localhost:3000/${id}.png`;
   try {
     const imgBuffer = fs.readFileSync(`uploads/${id}.png`);
     const descricao = await gerarDescricaoComGemini(imgBuffer);
 
-    const post = {
+    const post: Partial<postInterface>  = {
       ImgUrl: urlImagem,
       descricao: descricao,
       alt: req.body.alt,
     };
 
-    const postCriado = await atualizarPost(id, post);
+    const postCriado: UpdateResult = await atualizarPost(id, post);
     res.status(200).json(postCriado);
-  } catch (erro) {
+  } catch (erro: any) {
     console.error(erro.message);
     res.status(500).json({ Erro: "Falha na requisição" });
   }
 }
 
-export async function apagarPost(req, res) {
+export async function apagarPost(req: Request, res: Response): Promise<void> {
   const id = req.params.id;
   try {
-    excluirPost(id);
+    await excluirPost(id);
     res.status(200).json(`Post com id '${id}' foi deletado com sucesso$`);
-  } catch (erro) {
+  } catch (erro: any) {
     console.error(erro.message);
     res.status(500).json({ Erro: erro.message });
   }
--- src/models/postsModel.ts
+++ src/models/postsModel.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { ObjectId, MongoClient, Collection, InsertOneResult, UpdateResult, DeleteResult } from "mongodb"
 import conectarAoBanco from "../config/dbConfig.js";
 
-export interface Post {
+export interface postInterface {
   _id?: ObjectId;
   descricao: string;
   ImgUrl: string;
@@ -13,21 +13,21 @@ const conexao: MongoClient = await conectarAoBanco(process.env.STRING_CONEXAO!);
 // Seleciona o banco de dados
 const db = conexao.db("ImersaoAluraGoogle");
 // Seleciona a coleção "posts" dentro do banco de dados
-const colecao: Collection<Post> = db.collection("posts");
+const colecao: Collection<postInterface> = db.collection("posts");
 
 
 // Função assíncrona para buscar todos os posts do banco de dados
-export async function getTodosPosts(): Promise<Post[]> {
+export async function getTodosPosts(): Promise<postInterface[]> {
   // Retorna um array com todos os documentos da coleção
   const listaDePosts = colecao.find().toArray();
   return listaDePosts;
 }
 
-export async function criarPost(novoPost: Post): Promise<InsertOneResult<Post>> {
+export async function criarPost(novoPost: postInterface): Promise<InsertOneResult<postInterface>> {
   return colecao.insertOne(novoPost);
 }
 
-export async function atualizarPost(id: string, novoPost: Post): Promise<UpdateResult> {
+export async function atualizarPost(id: string, novoPost: Partial<postInterface>): Promise<UpdateResult> {
   const objID = ObjectId.createFromHexString(id);
   return colecao.updateOne({ _id: new ObjectId(objID) }, { $set: novoPost });
 }
